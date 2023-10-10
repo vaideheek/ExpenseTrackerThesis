9ca535d93b5b087a29efd6e89ea81f3dd4949b67
vaideheek <101187945+vaideheek@users.noreply.github.com>
2023-10-10 22:26:41 +0200

    Initial commit

---
+++ src/main/java/com/example/expensetrackerthesis/ExpenseTrackerThesisApplication.java
@@ -0,0 +1,13 @@
+package com.example.expensetrackerthesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerThesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerThesisApplication.class, args);
+	}
+
+}
